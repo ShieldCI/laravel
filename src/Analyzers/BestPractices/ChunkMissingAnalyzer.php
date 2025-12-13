c80a65ed6ca0151b6489ca96f58f60a80908740c
Haggai Wambua <haggai.wambua@gmail.com>
2025-12-12 23:21:19 -0500

    Implemented code snippet feature

--- src/Analyzers/BestPractices/ChunkMissingAnalyzer.php
+++ src/Analyzers/BestPractices/ChunkMissingAnalyzer.php
@@ -13,7 +13,6 @@ use ShieldCI\AnalyzersCore\Contracts\ResultInterface;
 use ShieldCI\AnalyzersCore\Enums\Category;
 use ShieldCI\AnalyzersCore\Enums\Severity;
 use ShieldCI\AnalyzersCore\ValueObjects\AnalyzerMetadata;
-use ShieldCI\AnalyzersCore\ValueObjects\Location;
 
 /**
  * Detects large dataset queries without chunking.
@@ -56,9 +55,10 @@ class ChunkMissingAnalyzer extends AbstractFileAnalyzer
                 $traverser->traverse($ast);
 
                 foreach ($visitor->getIssues() as $issue) {
-                    $issues[] = $this->createIssue(
+                    $issues[] = $this->createIssueWithSnippet(
                         message: $issue['message'],
-                        location: new Location($this->getRelativePath($file), $issue['line']),
+                        filePath: $file,
+                        lineNumber: $issue['line'],
                         severity: $issue['severity'],
                         recommendation: $issue['recommendation'],
                         code: $issue['code'] ?? null,
